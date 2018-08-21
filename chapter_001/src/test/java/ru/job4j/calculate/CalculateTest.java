9d2eacd8e294bb742583b6df64c54ee4dbb16ac6
paalse <mailto:paalse@list.ru>
2018-08-21 15:38:15 +0400

    text

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -5,25 +5,25 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Aleksey Pavlov (mailto:paalse@list.ru)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Aleksey Pavlov (mailto:paalse@list.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
 
 	/**
-	* Test echo.
-	*/
+	 * Test echo.
+	 */
 	@Test
-	
+
 	public void whenTakeNameThenTreeEchoPlusName() {
 		String input = "Aleksey Pavlov";
-		String expect = "Echo, echo, echo : Aleksey Pavlov"; 
+		String expect = "Echo, echo, echo : Aleksey Pavlov";
 		Calculate calc = new Calculate();
 		String result = calc.echo(input);
 		assertThat(result, is(expect));
 	}
- 
+
 }
