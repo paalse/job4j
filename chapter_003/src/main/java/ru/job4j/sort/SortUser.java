ff3efbb3df2f3245dd9a865ef569bc718960739c
paalse <mailto:paalse@list.ru>
2018-12-26 17:03:30 +0400

    2. Сортировка User с использованием Comparator [#10036]

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -1,8 +1,8 @@
 package ru.job4j.sort;
 
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.util.*;
 
 /**
  * Преобразование List в Set c сортировкой
@@ -10,9 +10,46 @@ import java.util.TreeSet;
  * @author Aleksey Pavlov (mailto:paalse@list.ru)
  */
 public class SortUser {
-    public Set<User> sort (List<User> list) {
+
+    /**
+     * Метод производит сортировку по полю age
+     *
+     * @param list - список для сортировки
+     * @return - отсортированный список
+     */
+    public Set<User> sort(List<User> list) {
         Set<User> result = new TreeSet<User>();
         result.addAll(list);
         return result;
+    }
+
+    /**
+     * Сортировка по длине имени.
+     *
+     * @return - отсортированный список
+     */
+    public List<User> sortNameLength(List<User> list) {
+        list.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getName().length() >= o2.getName().length() ? 1 : -1;
+            }
+        });
+        return list;
+    }
+
+    /**
+     * Сортировка в лексикографическом порядке по имени и по возрасту
+     *
+     * @return - отсортированный список
+     */
+    public List<User> sortByAllFields(List<User> list) {
+        list.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        return list;
     }
 }
--- chapter_003/src/test/java/ru/job4j/sort/UserTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/UserTest.java
@@ -16,14 +16,64 @@ import static org.junit.Assert.assertThat;
  * @author Aleksey Pavlov (mailto:paalse@list.ru)
  */
 public class UserTest {
+
+    /**
+     * Тест сортировки по возрасту
+     */
     @Test
-    public void sortTest() {
+    public void sortAgeTest() {
         List<User> users = new ArrayList<User>();
         users.add(new User("Alex", 25));
         users.add(new User("Petr", 5));
         users.add(new User("Ivan", 12));
 
-        TreeSet<User> result = new TreeSet<User>( new SortUser().sort(users));
-        assertThat(result.first().getAge(),is(5));
+        TreeSet<User> result = new TreeSet<User>(new SortUser().sort(users));
+        assertThat(result.first().getAge(), is(5));
+    }
+
+    /**
+     * Тест сортировки по длине имени
+     */
+    @Test
+    public void sortNameLengthTest() {
+        User row0 = new User("Aleksey", 25);
+        User row1 = new User("Ivan", 30);
+        User row2 = new User("Aleksey", 20);
+        User row3 = new User("Ivan", 25);
+        List<User> users = new ArrayList<User>();
+        users.add(row0);
+        users.add(row1);
+        users.add(row2);
+        users.add(row3);
+        List<User> expected = new ArrayList<User>();
+        expected.add(row1);
+        expected.add(row3);
+        expected.add(row0);
+        expected.add(row2);
+        List<User> result = new SortUser().sortNameLength(users);
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Тест сортировки по длине имени
+     */
+    @Test
+    public void sortByAllFieldsTest() {
+        User row0 = new User("Aleksey", 25);
+        User row1 = new User("Ivan", 30);
+        User row2 = new User("Aleksey", 20);
+        User row3 = new User("Ivan", 25);
+        List<User> users = new ArrayList<User>();
+        users.add(row0);
+        users.add(row1);
+        users.add(row2);
+        users.add(row3);
+        List<User> expected = new ArrayList<User>();
+        expected.add(row2);
+        expected.add(row0);
+        expected.add(row3);
+        expected.add(row1);
+        List<User> result = new SortUser().sortByAllFields(users);
+        assertThat(result, is(expected));
     }
 }
