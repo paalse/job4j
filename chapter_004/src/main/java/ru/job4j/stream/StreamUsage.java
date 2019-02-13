7389972223bc76ed50612a271f3d9f4de8480dc4
Aleksey Pavlov <paalse@list.ru>
2019-02-13 16:50:54 +0400

    0. Stream API [#58166]

---
+++ chapter_004/src/main/java/ru/job4j/stream/StreamUsage.java
@@ -0,0 +1,54 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Отфильтруем все задачи, где в имени есть слово BUG и сохраним ее в коллекцию List.
+ */
+public class StreamUsage {
+    public static class Task {
+        private final String name;
+        private final long spent;
+
+        public Task(String name, long spent) {
+            this.name = name;
+            this.spent = spent;
+        }
+
+        @Override
+        public String toString() {
+            return "Task{"
+                    + "name='" + name + '\''
+                    + ", spent=" + spent
+                    + '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Task> tasks = Arrays.asList(
+                new Task("Bug #1", 100),
+                new Task("Task #2", 100),
+                new Task("Bug #3", 100)
+        );
+
+        // Фильтрация, выводить только записи с "Bug"
+        List<Task> bugs = tasks.stream().filter(
+                task -> task.name.contains("Bug")
+        ).collect(Collectors.toList());
+        bugs.forEach(System.out::println);
+
+        //Преобразование, выводить только имена
+        List<String> names = tasks.stream().map(
+                task -> task.name
+        ).collect(Collectors.toList());
+        names.forEach(System.out::println);
+
+        //Упрощение, суммирует стоимоть
+        long total = tasks.stream().map(
+                task -> task.spent
+        ).reduce(0L, Long::sum);
+        System.out.println(total);
+    }
+}
