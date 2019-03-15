87924c28704f00f439e9c46726a9450b6839befa
Aleksey Pavlov <paalse@list.ru>
2019-03-15 09:41:21 +0400

    0. Видео[#119818]

---
+++ chapter_101/src/main/java/ru/job4j/services/IteratorArray.java
@@ -0,0 +1,23 @@
+package ru.job4j.services;
+
+import java.util.Iterator;
+
+public class IteratorArray implements Iterator {
+
+    private final int[] values;
+    private int index = 0;
+
+    public IteratorArray(final int[] values) {
+        this.values = values;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return values.length > index ;
+    }
+
+    @Override
+    public Object next() {
+        return values[index++];
+    }
+}
